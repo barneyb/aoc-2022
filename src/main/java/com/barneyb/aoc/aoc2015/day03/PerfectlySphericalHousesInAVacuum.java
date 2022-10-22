efd9e5bb5e826ae1e234ced07507b50a7814962b
Barney Boisvert <bboisvert@gmail.com>
2022-10-22 13:41:24 -0700

    2015 day 3 part two

--- src/main/java/com/barneyb/aoc/aoc2015/day03/PerfectlySphericalHousesInAVacuum.java
+++ src/main/java/com/barneyb/aoc/aoc2015/day03/PerfectlySphericalHousesInAVacuum.java
@@ -13,30 +13,48 @@ import java.util.HashSet;
  *
  * <pre>
  * {@link #PerfectlySphericalHousesInAVacuum(String)}
- * int {@link #getUniqueHousesVisited()}
+ * int {@link #getSoloUniqueHousesVisited()}
+ * int {@link #getPairedUniqueHousesVisited()}
  * </pre>
  */
 public class PerfectlySphericalHousesInAVacuum {
 
     @Getter
-    private final int uniqueHousesVisited;
+    private final int soloUniqueHousesVisited;
+
+    @Getter
+    private final int pairedUniqueHousesVisited;
 
     public PerfectlySphericalHousesInAVacuum(String input) {
-        val visited = new HashSet<Vec2>();
-        var curr = Vec2.origin();
-        visited.add(curr);
+        val soloVisited = new HashSet<Vec2>();
+        var solo = Vec2.origin();
+        soloVisited.add(solo);
+        val pairedVisited = new HashSet<Vec2>();
+        var meat = Vec2.origin();
+        var robo = Vec2.origin();
+        pairedVisited.add(meat);
+        pairedVisited.add(robo);
         input = input.trim();
         for (var i = 0; i < input.length(); i++) {
             val d = Dir.parse(input.charAt(i));
-            curr = curr.move(d);
-            visited.add(curr);
+            solo = solo.move(d);
+            soloVisited.add(solo);
+            if (i % 2 == 0) {
+                meat = meat.move(d);
+                pairedVisited.add(meat);
+            } else {
+                robo = robo.move(d);
+                pairedVisited.add(robo);
+            }
         }
-        uniqueHousesVisited = visited.size();
+        soloUniqueHousesVisited = soloVisited.size();
+        pairedUniqueHousesVisited = pairedVisited.size();
     }
 
     public static void main(String[] args) {
         Solver.execute(PerfectlySphericalHousesInAVacuum.class,
-                PerfectlySphericalHousesInAVacuum::getUniqueHousesVisited);
+                PerfectlySphericalHousesInAVacuum::getSoloUniqueHousesVisited,
+                PerfectlySphericalHousesInAVacuum::getPairedUniqueHousesVisited);
     }
 
 }
--- src/test/java/com/barneyb/aoc/aoc2015/day03/PerfectlySphericalHousesInAVacuumTest.java
+++ src/test/java/com/barneyb/aoc/aoc2015/day03/PerfectlySphericalHousesInAVacuumTest.java
@@ -6,23 +6,42 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class PerfectlySphericalHousesInAVacuumTest {
 
-    private void houseCount(int expected, String input) {
-        assertEquals(expected, new PerfectlySphericalHousesInAVacuum(input).getUniqueHousesVisited());
+    private void houseCountOne(int expected, String input) {
+        assertEquals(expected, new PerfectlySphericalHousesInAVacuum(input).getSoloUniqueHousesVisited());
     }
 
     @Test
     void partOne_exampleOne() {
-        houseCount(2, ">");
+        houseCountOne(2, ">");
     }
 
     @Test
     void partOne_exampleTwo() {
-        houseCount(4, "^>v<");
+        houseCountOne(4, "^>v<");
     }
 
     @Test
     void partOne_exampleThree() {
-        houseCount(2, "^v^v^v^v^v");
+        houseCountOne(2, "^v^v^v^v^v");
+    }
+
+    private void houseCountTwo(int expected, String input) {
+        assertEquals(expected, new PerfectlySphericalHousesInAVacuum(input).getPairedUniqueHousesVisited());
+    }
+
+    @Test
+    void partTwo_exampleOne() {
+        houseCountTwo(3, "^v");
+    }
+
+    @Test
+    void partTwo_exampleTwo() {
+        houseCountTwo(3, "^>v<");
+    }
+
+    @Test
+    void partTwo_exampleThree() {
+        houseCountTwo(11, "^v^v^v^v^v");
     }
 
 }
