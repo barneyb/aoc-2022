7e950541e22a76c96f8dae712ae32351f112079b
Barney Boisvert <bboisvert@gmail.com>
2022-10-22 08:29:50 -0700

    topo sort for 2018.07.1, but no tie breaking

---
+++ aoc2018/day07/aoc2018day07.go
@@ -0,0 +1,62 @@
+package day07
+
+import (
+	_ "embed"
+	"fmt"
+	"strings"
+	"unicode/utf8"
+
+	"github.com/barneyb/aoc-2022/util"
+)
+
+//go:embed input.txt
+var input_txt string
+
+// adjacency list
+type graph [][]int
+
+func (g graph) addEdge(a, b int) {
+	g[a] = append(g[a], b)
+}
+
+func (g graph) topoSort() []int {
+	visited := make([]bool, len(g))
+	order := make([]int, 0, len(g))
+	var walk func(int)
+	walk = func(u int) {
+		if visited[u] {
+			return
+		}
+		for _, v := range g[u] {
+			walk(v)
+		}
+		visited[u] = true
+		order = append(order, u)
+	}
+	for i := range g {
+		walk(i)
+	}
+	return order
+}
+
+func PartOne(input string, n int) string {
+	G := make(graph, n)
+	for _, line := range strings.Split(strings.TrimSpace(input), "\n") {
+		var as, bs string
+		util.Must(fmt.Sscanf(line,
+			"Step %1s must be finished before step %1s can begin.",
+			&bs, &as))
+		a, _ := utf8.DecodeRuneInString(as)
+		b, _ := utf8.DecodeRuneInString(bs)
+		G.addEdge(int(a-'A'), int(b-'A'))
+	}
+	return renderNodeList(G.topoSort())
+}
+
+func renderNodeList(ns []int) string {
+	result := make([]byte, 0, len(ns))
+	for _, n := range ns {
+		result = utf8.AppendRune(result, rune(n+'A'))
+	}
+	return string(result)
+}
---
+++ aoc2018/day07/aoc2018day07_test.go
@@ -0,0 +1,33 @@
+package day07
+
+import (
+	"testing"
+)
+
+const exampleOne = `
+Step C must be finished before step A can begin.
+Step C must be finished before step F can begin.
+Step A must be finished before step B can begin.
+Step A must be finished before step D can begin.
+Step B must be finished before step E can begin.
+Step D must be finished before step E can begin.
+Step F must be finished before step E can begin.
+`
+
+func TestPartOne(t *testing.T) {
+	tests := []struct {
+		in   string
+		n    int
+		want string
+	}{
+		{exampleOne, 6, "CABDFE"},
+	}
+	for _, c := range tests {
+		t.Run(c.want, func(t *testing.T) {
+			got := PartOne(c.in, c.n)
+			if got != c.want {
+				t.Errorf("PartOne gave %q, want %q", got, c.want)
+			}
+		})
+	}
+}
