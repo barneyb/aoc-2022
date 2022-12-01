c876bfa04ce5404a851510fa52cfeb83d604deb5
Barney Boisvert <bboisvert@gmail.com>
2022-12-01 13:48:44 -0800

    array is much faster than HashMap

--- src/main/java/com/barneyb/aoc/aoc2020/day15/RambunctiousRecitation.kt
+++ src/main/java/com/barneyb/aoc/aoc2020/day15/RambunctiousRecitation.kt
@@ -1,12 +1,13 @@
 package com.barneyb.aoc.aoc2020.day15
 
 import com.barneyb.aoc.util.Solver
+import java.util.*
 
 fun main() {
     Solver.execute(
         ::parse,
         { getNth(it, 2020) },
-        { getNth(it, 30000000) },
+        { getNth(it, 30_000_000) },
     )
 }
 
@@ -16,7 +17,8 @@ internal fun parse(input: String) =
         .map(String::toInt)
 
 internal fun getNth(seed: List<Int>, n: Int): Int {
-    val ages = HashMap<Int, Int>()
+    val ages = IntArray(n.coerceAtLeast(seed.max() + 1))
+    Arrays.fill(ages, -1)
     var curr = -1
     var priorTurn = -1
     for (turn in 0 until n) {
@@ -25,11 +27,10 @@ internal fun getNth(seed: List<Int>, n: Int): Int {
         } else if (priorTurn < 0) {
             curr = 0
         } else {
-            curr = ages[curr]!! - priorTurn
+            curr = ages[curr] - priorTurn
         }
-        priorTurn = ages[curr] ?: -1
+        priorTurn = ages[curr]
         ages[curr] = turn
     }
-    println("after $n turns, ${ages.size} values")
     return curr
 }
