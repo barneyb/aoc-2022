0058cc215af6f8301ccb8a562bcc9f819093b266
Barney Boisvert <bboisvert@gmail.com>
2022-12-06 08:47:30 -0800

    github action to test and run everything

---
+++ run_all.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+set -e
+
+cd "$(dirname "$0")"
+SCRIPT=$(basename "$0")
+if [[ $SCRIPT != .* ]]; then
+  cp "$SCRIPT" ".$SCRIPT"
+  exec "./.$SCRIPT" "$@"
+fi
+
+(
+    find src/main/java -name '*.java' -print0 \
+        | xargs -0 grep -l 'void main(' \
+        | sed -e 's/\.java$//'
+    find src/main/java -name '*.kt' -print0 \
+        | xargs -0 grep -l 'fun main(' \
+        | sed -e 's/\.kt$/Kt/'
+) \
+    | cut -c 15- \
+    | tr / . \
+    | grep '.aoc.' \
+    | sort \
+    | sed -e 's/\(.*\)/-Dexec.mainClass="\1"/' \
+    | xargs -n 1 mvn -q -B exec:java
+
+if [[ $SCRIPT = .* ]]; then
+  rm "$SCRIPT"
+fi
