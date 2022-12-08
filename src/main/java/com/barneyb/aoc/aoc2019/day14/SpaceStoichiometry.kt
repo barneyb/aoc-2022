b91eeff6867d85d0591351b769fc40bf6835df1d
Barney Boisvert <bboisvert@gmail.com>
2022-12-07 16:17:05 -0800

    2019.14p2 w/ binary search (vs hill climb)
    It's about ten times slower, since it has to do so much extra work. But
    still totally performant-enough, if we didn't have such nice guarantees
    from part one about how ORE is consumed to allow a one-shot hill climb.

--- src/main/java/com/barneyb/aoc/aoc2019/day14/SpaceStoichiometry.kt
+++ src/main/java/com/barneyb/aoc/aoc2019/day14/SpaceStoichiometry.kt
@@ -6,7 +6,8 @@ fun main() {
     Solver.benchmark(
         ::parse,
         { requiredOreForFuel(it, 1) },
-        { fuelFromOre(it, ONE_TRILLION) }
+        { fuelFromOre(it, ONE_TRILLION) },
+//        { fuelFromOre2(it, ONE_TRILLION) },
     )
 }
 
@@ -90,11 +91,28 @@ internal fun requiredOreForFuel(recipes: Recipes, neededFuel: Long): Long {
 internal fun fuelFromOre(recipes: Recipes, availOre: Long): Long {
     var fuel = 1L
     var ore = requiredOreForFuel(recipes, fuel)
-    while (ore < availOre) {
+    while (true) {
         val inc = (availOre - ore) * fuel / ore
         if (inc == 0L) break
         fuel += inc
         ore = requiredOreForFuel(recipes, fuel)
     }
     return fuel
+}
+
+@Suppress("unused") // play, not utility
+internal fun fuelFromOre2(recipes: Recipes, availOre: Long): Long {
+    // this is safe, since efficiency can only go UP w/ scale
+    var lo = availOre / requiredOreForFuel(recipes, 1)
+    var hi = availOre
+    var mid: Long
+    var ore: Long
+    while (true) {
+        mid = (lo + hi) / 2
+        if (mid == lo) break
+        ore = requiredOreForFuel(recipes, mid)
+        if (ore < availOre) lo = mid
+        else if (ore > availOre) hi = mid
+    }
+    return lo
 }
