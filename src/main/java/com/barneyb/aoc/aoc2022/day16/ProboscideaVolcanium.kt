105c56c9a9e86733be20ff2d79381cfded1a7001
Barney Boisvert <bboisvert@gmail.com>
2022-12-16 13:07:28 -0800

    Stack is faster, cleanup and restructure

--- src/main/java/com/barneyb/aoc/aoc2022/day16/ProboscideaVolcanium.kt
+++ src/main/java/com/barneyb/aoc/aoc2022/day16/ProboscideaVolcanium.kt
@@ -3,7 +3,7 @@ package com.barneyb.aoc.aoc2022.day16
 import com.barneyb.aoc.util.Solver
 import com.barneyb.aoc.util.toSlice
 import com.barneyb.util.HashMap
-import java.util.*
+import com.barneyb.util.Stack
 
 fun main() {
     Solver.execute(
@@ -19,21 +19,21 @@ internal data class Valve(
 )
 
 // Valve AA has flow rate=0; tunnels lead to valves DD, II, BB
-private val RE =
+private val lineMatcher =
     Regex("Valve ([A-Z]+) has flow rate=(\\d+); tunnels? leads? to valves? ([A-Z]+(, [A-Z]+)*)")
 
 internal fun parse(input: String) =
     input.toSlice()
         .trim()
         .lines()
-        .map(RE::matchEntire)
+        .map(lineMatcher::matchEntire)
         .map { m ->
             val (name, rate, tunnels) = m!!.groupValues.drop(1)
             Valve(name, rate.toInt(), tunnels.split(',').map(String::trim))
         }
 
 private data class Step(
-    val minute: Int,
+    val minutesLeft: Int,
     val valve: Valve,
     val projected: Int = 0,
     val open: Set<Valve> = emptySet(),
@@ -44,8 +44,8 @@ private data class Step(
 
     fun open() =
         copy(
-            minute = minute + 1,
-            projected = projected + valve.rate * (30 - minute),
+            minutesLeft = minutesLeft - 1,
+            projected = projected + valve.rate * minutesLeft,
             open = open + valve,
             rate = rate + valve.rate
         )
@@ -53,14 +53,14 @@ private data class Step(
     fun moveTo(v: Valve, dist: Int) =
         copy(
             valve = v,
-            minute = minute + dist,
+            minutesLeft = minutesLeft - dist,
         )
 
 }
 
-private const val START_ROOM = "AA"
+private const val START_VALVE = "AA"
 
-internal fun maximumPressureRelease(valves: List<Valve>): Int {
+private fun buildGraph(valves: List<Valve>): HashMap<Valve, MutableMap<Valve, Int>> {
     val index = HashMap<String, Valve>()
     for (v in valves) {
         index[v.name] = v
@@ -71,7 +71,7 @@ internal fun maximumPressureRelease(valves: List<Valve>): Int {
             .associateByTo(mutableMapOf(), index::get) { 1 }
     }
     for (v in valves) {
-        if (v.rate > 0 || v.name == START_ROOM) continue
+        if (v.rate > 0 || v.name == START_VALVE) continue
         val adj = adjacent[v]
         for ((a, ad) in adj) {
             for ((b, bd) in adj) {
@@ -87,35 +87,39 @@ internal fun maximumPressureRelease(valves: List<Valve>): Int {
         }
         adjacent.remove(v)
     }
-    println(buildString {
-        append("graph {\n")
-        for ((v, adj) in adjacent) {
-            append("  ${v.name} [label=\"${v.name} (${v.rate})\"]\n")
-            for ((o, d) in adj) {
-                if (v.name < o.name)
-                    append("  ${v.name} -- ${o.name} [label=$d]\n")
-            }
-        }
-        append("}\n")
-    })
-    val queue = PriorityQueue/*Queue*//*Stack*/(
-        Comparator.comparingInt(Step::projected).reversed()
-    )
-    queue.add(Step(1, index[START_ROOM]))
+//    println(buildString {
+//        append("graph {\n")
+//        for ((v, adj) in adjacent) {
+//            append("  ${v.name} [label=\"${v.name} (${v.rate})\"]\n")
+//            for ((o, d) in adj) {
+//                if (v.name < o.name)
+//                    append("  ${v.name} -- ${o.name} [label=$d]\n")
+//            }
+//        }
+//        append("}\n")
+//    })
+    return adjacent
+}
+
+internal fun maximumPressureRelease(valves: List<Valve>): Int {
+    val adjacent = buildGraph(valves)
+    val queue = Stack(Step(29, valves.first { it.name == START_VALVE }))
     val maxRate = valves.sumOf(Valve::rate)
     var best = Int.MIN_VALUE
     while (queue.isNotEmpty()) {
         val step = queue.remove()
-        if (step.minute > 30) continue
+        val remaining = step.minutesLeft
+        if (remaining < 0) continue
         if (step.projected > best) best = step.projected
-        if (step.minute == 30) continue
-        else if (step.projected + (30 - step.minute) * (maxRate - step.rate) < best) {
+        if (remaining == 0) continue
+        else if (step.projected + remaining * (maxRate - step.rate) < best) {
             continue
         }
-        if (step.canOpen())
+        if (remaining > 1 && step.canOpen())
             queue.add(step.open())
         for ((v, d) in adjacent[step.valve])
-            queue.add(step.moveTo(v, d))
+            if (remaining - d >= 1)
+                queue.add(step.moveTo(v, d))
     }
     return best
 }
