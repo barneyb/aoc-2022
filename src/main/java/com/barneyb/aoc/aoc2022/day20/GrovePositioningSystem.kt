f84379f29e9c07b055df5668f3ac0a5405e74b23
Barney Boisvert <bboisvert@gmail.com>
2022-12-20 13:14:37 -0800

    2022 day 20 part two

--- src/main/java/com/barneyb/aoc/aoc2022/day20/GrovePositioningSystem.kt
+++ src/main/java/com/barneyb/aoc/aoc2022/day20/GrovePositioningSystem.kt
@@ -2,24 +2,29 @@ package com.barneyb.aoc.aoc2022.day20
 
 import com.barneyb.aoc.util.Slice
 import com.barneyb.aoc.util.Solver
-import com.barneyb.aoc.util.toInt
+import com.barneyb.aoc.util.toLong
 import com.barneyb.aoc.util.toSlice
 
 fun main() {
     Solver.execute(
         ::parse,
         ::sumOfCoords, // 14888
+        ::fullDecryption, // 3760092545849
     )
 }
+
+internal const val DECRYPTION_KEY: Long = 811589153
+
+internal const val ROUNDS = 10
 
 internal fun parse(input: String) =
     input.toSlice()
         .trim()
         .lines()
-        .map(Slice::toInt)
+        .map(Slice::toLong)
 
 private class Node(
-    val value: Int,
+    val value: Long,
     var _prev: Node? = null,
     var _next: Node? = null,
 ) {
@@ -41,34 +46,51 @@ private class Node(
 
 }
 
-internal fun sumOfCoords(list: List<Int>): Int {
-    val nodes = Array(list.size) { Node(list[it]) }
+internal fun sumOfCoords(
+    list: List<Long>,
+    key: Long = 1,
+    rounds: Int = 1
+): Long {
+    val nodes = Array(list.size) { Node(list[it] * key) }
+//    println("Initial arrangement:")
+//    println(nodes.map(Node::value).joinToString(", "))
     nodes.first().prev = nodes.last()
     nodes.last().next = nodes.first()
+    var zero: Node? = if (nodes[0].value == 0L)
+        nodes[0]
+    else null
     for (i in 1 until nodes.size) {
+        if (nodes[i].value == 0L) zero = nodes[i]
         nodes[i].prev = nodes[i - 1]
         nodes[i - 1].next = nodes[i]
     }
-    var zero: Node? = null
-    for (n in nodes) {
-        if (n.value == 0) {
-            zero = n
-            continue
+//    println(draw(zero!!))
+    val countOfOthers = list.size - 1
+    val mid = list.size / 2
+    val negMid = -mid
+    repeat(rounds) {
+        for (n in nodes) {
+            var offset = (n.value % countOfOthers).toInt()
+            if (offset == 0) continue
+            else if (offset < negMid) offset += countOfOthers
+            else if (offset > mid) offset -= countOfOthers
+            var curr = n
+            if (offset < 0)
+                repeat(list.size - offset + 1) { curr = curr.prev }
+            else
+                repeat(offset) { curr = curr.next }
+            // pull it out of the ring
+            n.prev.next = n.next
+            n.next.prev = n.prev
+            // put it back after curr
+            n.prev = curr
+            n.next = curr.next
+            curr.next = n
+            n.next.prev = n
+//            println(draw(zero))
         }
-        // pull it out of the ring
-        n.prev.next = n.next
-        n.next.prev = n.prev
-        val offset = n.value
-        var curr = n
-        if (offset < 0) {
-            repeat(-offset + 1) { curr = curr.prev }
-        } else {
-            repeat(offset) { curr = curr.next }
-        }
-        n.prev = curr
-        n.next = curr.next
-        curr.next = n
-        n.next.prev = n
+//        println("\nAfter ${it + 1} round${if (it == 0) "" else "s"} of mixing:")
+//        println(draw(zero))
     }
     var curr = zero!!
     repeat(1000) { curr = curr.next }
@@ -79,3 +101,13 @@ internal fun sumOfCoords(list: List<Int>): Int {
     val three = curr.value
     return one + two + three
 }
+
+@Suppress("unused")
+private fun draw(zero: Node) =
+    generateSequence(zero) {
+        if (it.next == zero) null else it.next
+    }.map(Node::value)
+        .joinToString(", ")
+
+internal fun fullDecryption(list: List<Long>) =
+    sumOfCoords(list, DECRYPTION_KEY, ROUNDS)
--- src/test/java/com/barneyb/aoc/aoc2022/day20/GrovePositioningSystemKtTest.kt
+++ src/test/java/com/barneyb/aoc/aoc2022/day20/GrovePositioningSystemKtTest.kt
@@ -17,12 +17,25 @@ class GrovePositioningSystemKtTest {
 
     @Test
     fun parsing() {
-        assertEquals(listOf(1, 2, -3, 3, -2, 0, 4), parse(EXAMPLE_ONE))
+        assertEquals(listOf<Long>(1, 2, -3, 3, -2, 0, 4), parse(EXAMPLE_ONE))
     }
 
     @Test
     fun sumOfCoords() {
         assertEquals(3, sumOfCoords(parse(EXAMPLE_ONE)))
+    }
+
+    @Test
+    fun oneFullRound() {
+        assertEquals(
+            2434767459 + 1623178306 + 811589153,
+            sumOfCoords(parse(EXAMPLE_ONE), DECRYPTION_KEY)
+        )
+    }
+
+    @Test
+    fun fullDecrypt() {
+        assertEquals(1623178306, fullDecryption(parse(EXAMPLE_ONE)))
     }
 
 }
