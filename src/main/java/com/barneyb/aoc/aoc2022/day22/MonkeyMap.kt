8cf4531aa7fe57e6122e93fc221de8e1472ce6c2
Barney Boisvert <bboisvert@gmail.com>
2022-12-22 09:18:11 -0800

    restructure so "cross an edge" is pluggable

--- src/main/java/com/barneyb/aoc/aoc2022/day22/MonkeyMap.kt
+++ src/main/java/com/barneyb/aoc/aoc2022/day22/MonkeyMap.kt
@@ -3,86 +3,150 @@ package com.barneyb.aoc.aoc2022.day22
 import com.barneyb.aoc.util.Solver
 import com.barneyb.aoc.util.toSlice
 import com.barneyb.util.Dir
+import com.barneyb.util.HashMap
+import com.barneyb.util.Rect
 import com.barneyb.util.Vec2
 
 fun main() {
     Solver.execute(
         ::parse,
-        ::finalPassword, // 191,010
+        ::finalPasswordTorus, // 191,010
     )
 }
 
-private enum class Tile { OPEN, WALL }
+internal enum class Tile { OPEN, WALL }
 
-internal fun parse(input: String) =
-    input.toSlice().let { slice ->
-        var y = 1 // one-indexed
-        var x = 0
-        var curr: Vec2 = Vec2.ORIGIN
-        val tiles = HashMap<Vec2, Tile>()
-        var maxx = Int.MIN_VALUE
-        var heading = Dir.EAST
-        var n = 0
-        fun walk() {
-            if (n == 0) return
-            for (i in 0 until n) {
-                var next = curr.move(heading)
-                if (!tiles.containsKey(next)) {
-                    // wrap
-                    next = when (heading) {
-                        Dir.NORTH -> Vec2(next.x, y - 2)
-                        Dir.SOUTH -> Vec2(next.x, 1)
-                        Dir.EAST -> Vec2(1, next.y)
-                        Dir.WEST -> Vec2(maxx, next.y)
-                    }
-                    // traverse any empty space
-                    while (!tiles.containsKey(next))
-                        next = next.move(heading)
+internal enum class Turn {
+    LEFT,
+    RIGHT,
+    NONE; // sort of a kludge, but neatly sidesteps the fencepost problem
+
+    fun execute(d: Dir): Dir =
+        when (this) {
+            LEFT -> d.turnLeft()
+            RIGHT -> d.turnRight()
+            NONE -> d
+        }
+}
+
+internal data class Step(
+    val n: Int,
+    val dir: Turn,
+)
+
+internal data class Map(
+    val tiles: HashMap<Vec2, Tile>,
+    val bounds: Rect,
+    val topLeft: Vec2,
+    val steps: List<Step>,
+) {
+
+    fun contains(pos: Vec2) =
+        tiles.contains(pos)
+
+    operator fun get(pos: Vec2) =
+        tiles[pos]
+
+}
+
+internal data class State(
+    val pos: Vec2,
+    val facing: Dir,
+) {
+    val password
+        get() = pos.y * 1000 +
+                pos.x * 4 +
+                when (facing) {
+                    Dir.EAST -> 0
+                    Dir.SOUTH -> 1
+                    Dir.WEST -> 2
+                    Dir.NORTH -> 3
                 }
-                if (tiles[next] == Tile.WALL)
-                    break
-                curr = next
-            }
+}
+
+internal fun parse(input: String): Map {
+    var y = 1 // one-indexed
+    var x = 0
+    var start: Vec2 = Vec2.ORIGIN
+    val tiles = HashMap<Vec2, Tile>()
+    val steps = mutableListOf<Step>()
+    var maxx = Int.MIN_VALUE
+    var n = 0
+    fun walk(turn: Turn) {
+        if (n > 0) {
+            steps.add(Step(n, turn))
             n = 0
         }
-        for (c in slice) {
-            x++
-            when (c) {
-                '.' -> {
-                    if (curr === Vec2.ORIGIN)
-                        curr = Vec2(x, y)
-                    tiles[Vec2(x, y)] = Tile.OPEN
-                }
-                '#' ->
-                    tiles[Vec2(x, y)] = Tile.WALL
-                '\n' -> {
-                    if (n == 0) // still in the map
-                        maxx = maxx.coerceAtLeast(x)
-                    walk()
-                    x = 0
-                    y++
-                }
-                'R' -> {
-                    walk()
-                    heading = heading.turnRight()
-                }
-                'L' -> {
-                    walk()
-                    heading = heading.turnLeft()
-                }
-                in '0'..'9' ->
-                    n = n * 10 + c.digitToInt()
+    }
+    for (c in input.toSlice()) {
+        x++
+        when (c) {
+            '.' -> {
+                if (start === Vec2.ORIGIN)
+                    start = Vec2(x, y)
+                tiles[Vec2(x, y)] = Tile.OPEN
             }
+            '#' ->
+                tiles[Vec2(x, y)] = Tile.WALL
+            '\n' -> {
+                if (n == 0) // still in the map portion
+                    maxx = maxx.coerceAtLeast(x)
+                walk(Turn.NONE)
+                x = 0
+                y++
+            }
+            'R' ->
+                walk(Turn.RIGHT)
+            'L' ->
+                walk(Turn.LEFT)
+            in '0'..'9' ->
+                n = n * 10 + c.digitToInt()
         }
-        Pair(curr, heading)
+    }
+    return Map(
+        tiles,
+        Rect(
+            1,
+            1,
+            maxx - 1, // the newline
+            y - 3, // final newline, directions, blank
+        ),
+        start,
+        steps
+    )
+}
+
+private fun walk(map: Map, crossEdge: (State) -> State) =
+    map.steps.fold(State(map.topLeft, Dir.EAST)) { state, (n, turn) ->
+        var (curr, facing) = state
+        for (i in 0 until n) {
+            var next = curr.move(facing)
+            if (!map.contains(next)) {
+                crossEdge(State(next, facing)).also {
+                    next = it.pos
+                    facing = it.facing
+                }
+            }
+            if (map[next] == Tile.WALL)
+                break
+            curr = next
+        }
+        State(curr, turn.execute(facing))
     }
 
-fun finalPassword(state: Pair<Vec2, Dir>) =
-    state.first.y * 1000 +
-            state.first.x * 4 +
-            when (state.second) {
-                Dir.NORTH -> 3
-                Dir.SOUTH -> 1
-                Dir.EAST -> 0
-                Dir.WEST -> 2
-            }
+internal fun finalPasswordTorus(map: Map) =
+    walk(map) { (pos, facing) ->
+        // wrap
+        var next = when (facing) {
+            Dir.NORTH -> Vec2(pos.x, map.bounds.y2)
+            Dir.SOUTH -> Vec2(pos.x, map.bounds.y1)
+            Dir.EAST -> Vec2(map.bounds.x1, pos.y)
+            Dir.WEST -> Vec2(map.bounds.x2, pos.y)
+        }
+
+        // traverse any empty spaces
+        while (!map.contains(next))
+            next = next.move(facing)
+
+        State(next, facing)
+    }.password
--- src/test/java/com/barneyb/aoc/aoc2022/day22/MonkeyMapKtTest.kt
+++ src/test/java/com/barneyb/aoc/aoc2022/day22/MonkeyMapKtTest.kt
@@ -1,6 +1,6 @@
 package com.barneyb.aoc.aoc2022.day22
 
-import com.barneyb.util.Dir
+import com.barneyb.util.Rect
 import com.barneyb.util.Vec2
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
@@ -25,12 +25,22 @@ class MonkeyMapKtTest {
 
     @Test
     fun parsing() {
-        assertEquals(Pair(Vec2(8, 6), Dir.EAST), parse(EXAMPLE_ONE))
+        val map = parse(EXAMPLE_ONE)
+        assertEquals(Vec2(9, 1), map.topLeft)
+        assertEquals(
+            Rect(
+                1..16,
+                1..12,
+            ),
+            map.bounds
+        )
+        assertEquals(96, map.tiles.size)
+        assertEquals(7, map.steps.size)
     }
 
     @Test
     fun exampleOne() {
-        assertEquals(6032, finalPassword(parse(EXAMPLE_ONE)))
+        assertEquals(6032, finalPasswordTorus(parse(EXAMPLE_ONE)))
     }
 
 }
