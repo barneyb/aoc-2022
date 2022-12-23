2fa6e6daaa8b3051e42eb3dbff2303ff16a04420
Barney Boisvert <bboisvert@gmail.com>
2022-12-22 20:35:29 -0800

    dynamically fold into a torus

--- src/main/java/com/barneyb/aoc/aoc2022/day22/Map.kt
+++ src/main/java/com/barneyb/aoc/aoc2022/day22/Map.kt
@@ -26,12 +26,18 @@ internal class Map(
         assert(topLeft.x % it == 1) { "$it leaves $topLeft not in a corner" }
     }
 
-    private var pairs: HashMap<Edge, Edge>? = null
+    private var adjacentEdges: HashMap<Edge, Edge>? = null
 
     inner class Edge(
         val square: Int, // 0..15, on a 4x4 grid
         val dir: Dir,
     ) {
+
+        /**
+         * The corner where the edge starts, moving clockwise. E.g., a NORTH
+         * edge will start at the top-left of the square, and a SOUTH edge will
+         * start at the bottom-right.
+         */
         val start: Vec2 = when (dir) {
             NORTH -> Vec2(1 + leg * (square % 4), 1 + leg * (square / 4))
             EAST -> Vec2(leg * (square % 4 + 1), 1 + leg * (square / 4))
@@ -39,6 +45,9 @@ internal class Map(
             WEST -> Vec2(1 + leg * (square % 4), leg * (square / 4 + 1))
         }
 
+        /**
+         * The range of the dynamic coordinate from [start] along the edge.
+         */
         val range = when (dir) {
             NORTH -> start.x until start.x + leg
             SOUTH -> start.x downTo start.x - leg + 1
@@ -46,6 +55,7 @@ internal class Map(
             WEST -> start.y downTo start.y - leg + 1
         }
 
+        /** Whether the given point is on this edge. */
         fun contains(p: Vec2) =
             when (dir) {
                 NORTH -> p.x in range && p.y == start.y
@@ -54,6 +64,9 @@ internal class Map(
                 WEST -> p.x == start.x && p.y in range
             }
 
+        /**
+         * Cross to the other (adjacent) edge, and return the resulting [State].
+         */
         fun crossTo(other: Edge, pos: Vec2): State {
             fun map(c: Int) =
                 other.range.reversed()[range.indexOf(c)]
@@ -71,6 +84,11 @@ internal class Map(
                 other.dir.reversed()
             )
         }
+
+        override fun toString(): String {
+            return "Edge(dir=$dir, start=$start, range=$range)"
+        }
+
     }
 
     fun contains(pos: Vec2) =
@@ -80,37 +98,41 @@ internal class Map(
         tiles[pos]
 
     fun foldIntoTorus() {
-        pairs = HashMap<Edge, Edge>().apply {
-            when (leg) {
-                4 -> listOf(
-                    Edge(2, NORTH) to Edge(10, SOUTH),
-                    Edge(4, NORTH) to Edge(4, SOUTH),
-                    Edge(2, EAST) to Edge(2, WEST),
-                    Edge(11, EAST) to Edge(10, WEST),
-                    Edge(6, EAST) to Edge(4, WEST),
-                    Edge(11, NORTH) to Edge(11, SOUTH),
-                    Edge(5, NORTH) to Edge(5, SOUTH),
-                )
-                50 -> listOf(
-                    Edge(1, NORTH) to Edge(9, SOUTH),
-                    Edge(12, WEST) to Edge(12, EAST),
-                    Edge(2, NORTH) to Edge(2, SOUTH),
-                    Edge(12, SOUTH) to Edge(8, NORTH),
-                    Edge(2, EAST) to Edge(1, WEST),
-                    Edge(9, EAST) to Edge(8, WEST),
-                    Edge(5, EAST) to Edge(5, WEST),
-                )
-                else ->
-                    throw IllegalStateException("Unsupported $leg-length leg?!")
-            }.forEach {
-                put(it.first, it.second)
-                put(it.second, it.first)
+        adjacentEdges = HashMap<Edge, Edge>().apply {
+            fun walk(
+                dir: Dir,
+                pt: (major: Int, minor: Int) -> Vec2,
+                sqr: (major: Int, minor: Int) -> Int,
+            ) {
+                for (major in 0 until 4) {
+                    var first: Int? = null
+                    var last: Int? = null
+                    for (minor in 0 until 4) {
+                        val p = pt(
+                            1 + major * leg,
+                            1 + minor * leg
+                        )
+                        if (tiles.contains(p)) {
+                            if (first == null)
+                                first = minor
+                            last = minor
+                        }
+                    }
+                    if (first != null) { // square(s) in this row/column exist!
+                        val a = Edge(sqr(major, first), dir.reversed())
+                        val z = Edge(sqr(major, last!!), dir)
+                        put(a, z)
+                        put(z, a)
+                    }
+                }
             }
+            walk(EAST, { y, x -> Vec2(x, y) }, { y, x -> y * 4 + x })
+            walk(SOUTH, { x, y -> Vec2(x, y) }, { x, y -> y * 4 + x })
         }
     }
 
     fun foldIntoCube() {
-        pairs = HashMap<Edge, Edge>().apply {
+        adjacentEdges = HashMap<Edge, Edge>().apply {
             when (leg) {
                 4 -> listOf(
                     Edge(2, NORTH) to Edge(4, NORTH),
@@ -141,9 +163,10 @@ internal class Map(
 
     fun crossEdge(state: State): State {
         val (pos, facing) = state
-        val (edge, other) = pairs!!.first { (k, _) ->
+        val (edge, other) = adjacentEdges!!.firstOrNull { (k, _) ->
             k.contains(pos) && k.dir == facing
         }
+            ?: throw IllegalArgumentException("Unknown edge facing $facing from $pos.")
         return edge.crossTo(other, pos)
     }
 }
